ec10c241d41e8b3a0550b2a78e713b5dbc849039
kdcheng10 <kdcheng10@gmail.com>
2021-07-16 01:13:18 -0500

    no message

---
+++ 611.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+  public int triangleNumber(int[] nums) {
+    if (nums.length < 3)
+      return 0;
+    int result = 0;
+    Arrays.sort(nums);
+
+    for (int i = 0; i < nums.length - 2; i++) {
+      if (nums[i] == 0)
+        continue;
+      int k = i + 2;
+      for (int j = i + 1; j < nums.length - 1; j++) {
+        while (k < nums.length && nums[i] + nums[j] > nums[k]) {
+          k++;
+        }
+        result += (k - j - 1);
+      }
+    }
+
+    return result;
+  }
+}
